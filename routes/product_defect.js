70d809fb1a06d54922e55ba7a87dc89602957e93
rizkiazkafihiaghnia996 <rizkiazkafihiaghnia996@gmail.com>
2023-03-31 10:56:39 +0700

    ...

--- routes/product_defect.js
+++ routes/product_defect.js
@@ -2,12 +2,12 @@ const productDefectRoute = require('express').Router();
 const { ProductDefectController } = require('../controllers');
 
 productDefectRoute.get('/', ProductDefectController.getProductDefect);
-productDefectRoute.get('/detailed', ProductDefectController.getProductsDefects);
-productDefectRoute.get("/add", ProductDefectController.addProductDefectPage)
 productDefectRoute.post('/add', ProductDefectController.addProductDefect);
 productDefectRoute.get('/delete/:id', ProductDefectController.deleteProductDefect);
-productDefectRoute.get('/product/search/:id', ProductDefectController.getDefectByProduct)
-productDefectRoute.get('/update/:id', ProductDefectController.updateProductDefectPage)
-productDefectRoute.post('/update/:id', ProductDefectController.updateProductDefect)
+productDefectRoute.get('/add', ProductDefectController.addProductDefectPage);
+productDefectRoute.get('/product/search/:id', ProductDefectController.getDefectByProduct);
+productDefectRoute.get('/update/:id', ProductDefectController.updateProductDefectPage);
+productDefectRoute.post('/update/:id', ProductDefectController.updateProductDefect);
+productDefectRoute.get('/detailed', ProductDefectController.getProductsDefects);
 
 module.exports = productDefectRoute
